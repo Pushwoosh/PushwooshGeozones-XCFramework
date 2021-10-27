d85cd91e349bed6132692b2e3c9baac80a70c73f
Andrey Kiselev <akiselev@pushwoosh.com>
2021-10-27 16:27:19 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PushwooshGeozones",
+    products: [
+        .library(
+            name: "PushwooshGeozones",
+            targets: ["PushwooshGeozones"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "PushwooshGeozones",
+            url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/6.2.6/PushwooshGeozones.xcframework.zip",
+            checksum: "4be57bc530721846d4bdcffc52562e0fc23b38e349e6b139600aea26cac3cf2f"
+        )
+    ]
+)
