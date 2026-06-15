b3a0b5fdf7deb75840d384406879334dd2860ad6
pushwoosh <pushwoosh@PushwoosMini001.ovb>
2026-06-15 19:30:03 +0700

    Release 7.1.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PushwooshGeozones",
-            url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/7.0.46/PushwooshGeozones.xcframework.zip",
-            checksum: "cafac8ab33be2c340982dc5ce07635d7f5d360e63250474042c7c9c1de875515"
+            url: "https://github.com/Pushwoosh/pushwoosh-ios-sdk/releases/download/7.1.0/PushwooshGeozones.xcframework.zip",
+            checksum: "1dd79cedd742ff791ad86ccb32ca05b7362b48805c2f6efe41bf709ee5572797"
         )
     ]
 )
